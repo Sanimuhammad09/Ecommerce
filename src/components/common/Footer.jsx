993085179fee4032797808eb61f04bf711312e01
sanimuhammad09 <muhammad@zaktech.ng>
2023-07-04 03:25:21 +0100

    edit names

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           <span>
             Developed by
             {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+            <a href="https://github.com/sanimuhammad09">Zenqeeth Solutions</a>
           </span>
         </strong>
       </div>
@@ -33,7 +33,7 @@ const Footer = () => {
         <strong>
           <span>
             Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+            <a href="https://github.com/Sanimuhammad09/Ecommerce">HERE</a>
           </span>
         </strong>
       </div>
